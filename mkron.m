2e0513883870c6fad20fa24a363bc60848a1e846
Kim Batselier <kim.batselier@gmail.com>
2014-09-12 19:36:22 +0800

    mkron now accepts scalar input for repeated Kronecker product of 1 vector.

--- mkron.m
+++ mkron.m
@@ -1,17 +1,21 @@
 function y=mkron(varargin)
-% y=mkron(x1,x2,x3,...)
-% ---------------------
+% y=mkron(x1,x2,x3,...) or y=mkron(x1,a)
+% --------------------------------------
 % Returns a Kronecker product of multiple input matrices x1,x2,x3... Needs
-% at least 2 input arguments.
+% at least 2 input arguments. Alternatively, one can provide one matrix x1
+% and a scalar a to compute the repeated Kronecker product of x1 with
+% itself.
 %
 % y         =   matrix, Kronecker product of x1,x2,x3..
 %
 % x1,2,3,.. =   matrix, matrix of arbitrary size.
-
+%
+% a         =   scalar, only used when given with 1 matrix input argument.
+%
 % Reference
 % ---------
 %
-% 2014, Kim Batselier
+% 08/2014, Kim Batselier
 
 n=length(varargin);
 
@@ -19,9 +23,19 @@ if n<2
     error('Need at least 2 input arguments.')
 end
 
-y=varargin{1};
-for i=2:n
-    y=kron(y,varargin{i});
+% check whether second argument is a scalar, if so, then repeatedly apply
+% kronecker product onto first argument
+if isscalar(varargin{2})
+    n=varargin{2};
+    y=varargin{1};
+    for i=2:n
+        y=kron(y,varargin{1});
+    end    
+else
+    y=varargin{1};
+    for i=2:n
+        y=kron(y,varargin{i});
+    end
 end
 
 end
